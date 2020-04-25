87a68d7ace1a4306d6d4b49791742b4bd02ecc81
Ful Ful <rzezbi1@gmail.com>
2020-04-25 19:24:05 +0200

    Add controllers: - IndexController - OwnerController - VetController
    Add html pages for:
    - Index
    - Owners
    - Vets

---
+++ pet-clinic-web/src/main/java/pl/fulful/com/petclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package pl.fulful.com.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html"})
+    public String allVets(Model model) {
+
+        return "vets/index";
+    }
+}
