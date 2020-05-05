2987690342cd511e240d5ad8943f99b6c3940371
Ful Ful <rzezbi1@gmail.com>
2020-05-05 19:56:56 +0200

    show all owners in owners index page

--- pet-clinic-web/src/main/java/pl/fulful/com/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/fulful/com/petclinic/controllers/OwnerController.java
@@ -3,13 +3,22 @@ package pl.fulful.com.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.fulful.com.petclinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String allOwners(Model model) {
+//        model.addAttribute("owners_param", ownerService.findAll().stream().collect(Collectors.toList()));
+        model.addAttribute("owners_param", ownerService.findAll());
 
         return "owners/index";
     }
