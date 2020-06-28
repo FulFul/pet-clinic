1a94bd8cb79b8cb4c35c1d11305ff36e0bfae208
Ful Ful <rzezbi1@gmail.com>
2020-06-28 17:41:08 +0200

    07_31     - added notFound page     - link notFound page to /oups     - link vets link to vets.html

--- pet-clinic-web/src/main/java/pl/fulful/com/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/fulful/com/petclinic/controllers/VetController.java
@@ -14,7 +14,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html", "vets.html"})
     public String allVets(Model model) {
         model.addAttribute("vets_param", vetService.findAll());
 
