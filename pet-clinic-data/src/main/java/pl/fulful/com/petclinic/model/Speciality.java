f662cd8d87d2dc1e7299e196171ee887600256a2
Ful Ful <rzezbi1@gmail.com>
2020-07-02 13:39:56 +0200

    08_20     - Speciality & Vet mods - @Entity modifications

--- pet-clinic-data/src/main/java/pl/fulful/com/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/pl/fulful/com/petclinic/model/Speciality.java
@@ -1,8 +1,19 @@
 package pl.fulful.com.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
+
+    @ManyToMany(mappedBy = "specialities")
+    private Set<Vet> vets = new HashSet<>();
 
     public String getDescription() {
         return description;
@@ -10,5 +21,11 @@ public class Speciality extends BaseEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public void addVet(Vet vet) {
+        // TODO: nie jestem pewien czy to poniżej jest konieczne
+        vet.addSpeciality(this);
+        this.vets.add(vet);
     }
 }
--- pet-clinic-data/src/main/java/pl/fulful/com/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/pl/fulful/com/petclinic/model/Vet.java
@@ -1,10 +1,18 @@
 package pl.fulful.com.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "vet_speciality",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name = "speciality_id")
+    )
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
@@ -13,5 +21,11 @@ public class Vet extends Person {
 
     public void setSpecialities(Set<Speciality> specialities) {
         this.specialities = specialities;
+    }
+
+    public void addSpeciality(Speciality speciality) {
+        // TODO: nie jestem pewien czy to poniżej jest konieczne
+        speciality.addVet(this);
+        this.specialities.add(speciality);
     }
 }
