1e9f55909e7f9c479a02ae5c61e280015ad96807
Ful Ful <rzezbi1@gmail.com>
2020-07-02 13:22:41 +0200

    08_18 - 08_19     - @MappedSuperclass added to BaseEntity & Person     - modified Owner, Person, Pet, PetType entities

--- pet-clinic-data/src/main/java/pl/fulful/com/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/pl/fulful/com/petclinic/model/Pet.java
@@ -1,13 +1,28 @@
 package pl.fulful.com.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @Column(name = "pet_type_enum")
+    @Enumerated(value = EnumType.STRING)
     private PetTypeEnum petTypeEnum;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
--- pet-clinic-data/src/main/java/pl/fulful/com/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pl/fulful/com/petclinic/model/PetType.java
@@ -1,8 +1,20 @@
 package pl.fulful.com.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+    @OneToMany(mappedBy = "petType")
+    private Set<Pet> pets = new HashSet<>();
 
     public String getName() {
         return name;
